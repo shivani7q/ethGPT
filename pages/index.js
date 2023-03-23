433cf08e839805280ef4fb6e87a1d09ef83d87bd
Shivani Singh <94933127+shivani7q@users.noreply.github.com>
2023-03-23 13:40:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import ethlogo from 'https://www.pngall.com/wp-content/uploads/10/ethereum-logo-png-images.png';
+import ethlogo from '/ethereum.png';
 import { useState } from 'react';
 
 
